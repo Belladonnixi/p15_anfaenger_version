83a14076e5003ba09bae14967b770cfc80f32d75
Belladonnixi <jessica_ernst@outlook.de>
2025-01-09 10:36:08 +0100

    add import to switch

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:p15_anfaenger_version/listview_builder_version.dart'; // diese Zeile auskommentieren um die ListView.builder Version zu verwenden
+import 'package:p15_anfaenger_version/listview_builder_version.dart';
+// import 'package:p15_anfaenger_version/listview_version.dart'; // diese Zeile auskommentieren um die ListView.builder Version zu verwenden
 
 // Die Hauptfunktion, die die App startet.
 void main() {
-  // MainApp2 ist die Klasse, die die ListView.builder Version enthält hier also ändern auf MainApp2
+  // MainApp ist die Klasse, die die ListView Version enthält hier also ändern auf MainApp
+  // wenn man die ausführen will
   runApp(const MainApp2());
 }
 
